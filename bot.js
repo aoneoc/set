cbc1020b36ce638a601da316af7623887dc84ffe
aoneoc <55067568+aoneoc@users.noreply.github.com>
2019-10-21 16:22:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["320015606071951360"] :point_left:
+const developers = ["320015606071951360"]
 const adminprefix = "&";
 client.on('message', message => {
     var argresult = message.content.split( ).slice(1).join(' ');
